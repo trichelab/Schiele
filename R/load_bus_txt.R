817a9cd1be7fb7e0f0569e07ece64c38c70eb412
Tim Triche, Jr <tim.triche@gmail.com>
2023-04-19 14:34:09 -0400

    add BUS file parsing demo and function

---
+++ R/load_bus_txt.R
@@ -0,0 +1,31 @@
+#' Read a .bus.txt file produced by `bustools text`
+#' 
+#' This is rarely done in practice (BUS is more efficient) but useful for demos.
+#'
+#' @param   file      the bustools text file to import
+#' 
+#' @return            a data.frame 
+#'
+#' @details
+#' This function works fine on gzipped files too.
+#' 
+#' @examples 
+#' bus_txt <- system.file("extdata", "short.bus.txt.gz", package="Schiele")
+#' bus <- load_bus_txt(bus_txt)
+#' 
+#' # how many fragments per cell? (low-pass CITEseq example)
+#' summary(as.integer(with(bus, rowsum(count, barcode))))
+#' 
+#' # how many fragments per equivalence class (feature)?
+#' summary(as.integer(with(bus, rowsum(count, set))))
+#'
+#' # how many equivalence classes (features) per cell?
+#' summary(sapply(split(bus, bus$barcode), function(x) length(unique(x$set))))
+#'
+#' @export
+load_bus_txt <- function(file) {
+
+  read.table(file, header=FALSE, sep="\t", stringsAsFactors=FALSE, 
+             col.names=c("barcode","umi","set","count"))
+
+}
