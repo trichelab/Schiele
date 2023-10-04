055b1c0ef61e647a1fc9513db03d9b92d2539fd0
Tim Triche, Jr <tim.triche@gmail.com>
2023-10-04 12:57:12 -0400

    make markKept() work similarly to kneePlot()

--- R/markKept.R
+++ R/markKept.R
@@ -8,7 +8,8 @@
 #' @param sce           a SingleCellExperiment object
 #' @param targetCells   the number of cells supposedly loaded (20000)
 #' @param targetUMIs    minimum UMIs to aim for if UMIs[targetCells] is low (50)
-#' @param ...           additional arguments to be passed to emptyDrops
+#' @param column        optional colData name for cached results ("UMIs")
+#' @param ...           additional arguments to perhaps pass to emptyDrops
 #' 
 #' @return a SingleCellExperiment object with a new column `keep`
 #'
@@ -16,10 +17,12 @@
 #' @seealso emptyDrops
 #' 
 #' @export
-markKept <- function(sce, targetCells=20000, targetUMIs=50, ...) {
+markKept <- function(sce, targetCells=20000, targetUMIs=50, column="UMIs", ...){
 
-  sce$UMIs <- colSums(counts(sce))
-  tally <- sort(sce$UMIs, decreasing=TRUE)
+  if (!column %in% names(colData(sce))) {
+    colData(sce)[[column]] <- colSums(counts(sce))
+  }
+  tally <- sort(colData(sce)[[column]], decreasing=TRUE)
   sat <- max(which(tally >= targetUMIs))
   intercept <- tally[targetCells]
   if (targetUMIs > intercept) {
@@ -27,11 +30,11 @@ markKept <- function(sce, targetCells=20000, targetUMIs=50, ...) {
             ") at targetCells!")
     message("Only ", sat, " cells have at least ", targetUMIs, " UMIs.")
     message("Setting `keep` to TRUE for these cells.")
-    sce$keep <- (sce$UMIs >= targetUMIs)
+    sce$keep <- (colData(sce)[[column]] >= targetUMIs)
   } else { 
     message("Your UMI count at targetCells is ", intercept, ".")
     message("You might consider running emptyDrops() on this dataset.")
-    sce$keep <- (sce$UMIs >= intercept)
+    sce$keep <- (colData(sce)[[column]] >= intercept)
   }
 
   return(sce)
